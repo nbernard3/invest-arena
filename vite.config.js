7c037442fb3a59e09c5274e16ea2c108cb5e09a9
nbernard3 <nicolas.bernard3@gmail.com>
2025-01-28 21:03:11 +0100

    updayed vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' 
-  ? '/stratinvest-arena/' 
+  ? '/invest-arena/' 
   : '/',
   plugins: [
     vue(),
